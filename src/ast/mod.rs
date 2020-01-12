92a074ca64e4889a5bedd91f34a04caad8fae91e
Theo Felippe <iam@tfelippe.com>
2020-01-12 13:43:33 +0000

    adding ast

---
+++ src/ast/mod.rs
@@ -0,0 +1,13 @@
+pub trait Node {
+    fn token_literal(&self) -> String;
+}
+
+pub trait Statement: Node {
+    fn statement_node(&self);
+}
+
+pub trait Expression: Node {
+    fn expression_node(&self);
+}
+
+pub struct Program {}
